7fb0cf39117327c5ae0c9f1edecab93876e38779
leo989 <leo.coaccioli@gmail.com>
2013-06-08 21:32:52 +0200

    piccole modifiche estetiche

--- 2013-06-07/javascript/exercise4.js
+++ 2013-06-07/javascript/exercise4.js
@@ -4,11 +4,11 @@ var altezza = 2;
 var fattore_di_scala = [0.5,0.5,0.1];
 var parti = 40;
 
-var verde = [0,1,0];
+var verde = [0.13,0.54,0.13];
 var marrone = [0.54,0.27,0.07];
-var nocciola = [0.85,0.65,0.12];
-var nero = [0,0,0];
-var celeste = [0.4,0.8,0.67];
+var nocciola = [0.72,0.52,0.04];
+var nero = [0.11,0.11,0.11];
+var celeste = [0.5,1,0.83,0.6];
 
 function generaNumero(min, max) {
   return Math.random() * (max - min) + min;
